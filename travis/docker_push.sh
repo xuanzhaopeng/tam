0656fd064416f465f2a227143fa14114615bdbf8
Zhaopeng Xuan <zhaopeng.xuan@veon.com>
2019-01-15 22:44:13 +0100

    Fix travis

--- travis/docker_push.sh
+++ travis/docker_push.sh
@@ -4,3 +4,8 @@ set -e
 
 echo $TRAVIS_REPO_SLUG
 echo $1
+
+docker build -t $TRAVIS_REPO_SLUG .
+docker tag $TRAVIS_REPO_SLUG $TRAVIS_REPO_SLUG:$1
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push $TRAVIS_REPO_SLUG:$1
